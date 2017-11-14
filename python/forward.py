51ec30e17d962da55846d17d51d2d455e7602190
jingpengwu <jingpeng.wu@gmail.com>
2017-11-14 15:41:14 -0500

    fix test error

--- python/forward.py
+++ python/forward.py
@@ -6,77 +6,80 @@ Inference.
 Kisuk Lee <kisuklee@mit.edu>, 2016
 """
 
-import ConfigParser
 import caffe
 import h5py
-import os
 import sys
 import time
 
 import config
 from DataProvider.python.forward import ForwardScanner
 
-# Initialize.
-caffe.set_device(int(sys.argv[1]))
-caffe.set_mode_gpu()
 
-# Forward config.
-cfg = config.ForwardConfig(sys.argv[2])
+def forward(config_file, device_id = 0):
+    # Initialize.
+    caffe.set_device( device_id )
+    caffe.set_mode_gpu()
 
-# Create an inference net.
-net = cfg.net()
+    # Forward config.
+    cfg = config.ForwardConfig( config_file )
 
-# Create net spec.
-net_spec = dict()
-for i in net.inputs:
-    net_spec[i] = net.blobs[i].data.shape[-3:]
+    # Create an inference net.
+    net = cfg.net()
 
-# Create data provider.
-dp = cfg.get_data_provider(net_spec)
+    # Create net spec.
+    net_spec = dict()
+    for i in net.inputs:
+        net_spec[i] = net.blobs[i].data.shape[-3:]
 
-# Scan params.
-scan_list   = eval(cfg.get('forward','scan_list'))
-scan_params = eval(cfg.get('forward','scan_params'))
-save_prefix = cfg.get('forward','save_prefix')
+    # Create data provider.
+    dp = cfg.get_data_provider(net_spec)
 
-# Create scan spec.
-scan_spec = dict()
-for i in scan_list:
-    scan_spec[i] = net.blobs[i].data.shape[-4:]
+    # Scan params.
+    scan_list   = eval(cfg.get('forward', 'scan_list'))
+    scan_params = eval(cfg.get('forward', 'scan_params'))
+    save_prefix = cfg.get('forward','save_prefix')
 
-# Forward scan.
-for dataset in dp.datasets:
-    idx = dataset.params['dataset_id']
-    print 'Forward scan dataset {}'.format(idx)
+    # Create scan spec.
+    scan_spec = dict()
+    for i in scan_list:
+        scan_spec[i] = net.blobs[i].data.shape[-4:]
 
-    # Create ForwardScanner for the current dataset.
-    fs = ForwardScanner(dataset, scan_spec, params=scan_params)
+    # Forward scan.
+    for dataset in dp.datasets:
+        idx = dataset.params['dataset_id']
+        print 'Forward scan dataset {}'.format(idx)
 
-    # Scan loop.
-    ins = fs.pull()  # Fetch initial inputs.
-    while ins is not None:
-        start = time.time()
-        # Set inputs.
-        for k, v in ins.iteritems():
-            shape = (1,) + v.shape
-            net.blobs[k].reshape(*shape)
-            net.blobs[k].data[0,...] = v
-        # Run forward pass.
-        net.forward()
-        # Extract output data.
-        outs = dict()
-        for k in scan_spec.iterkeys():
-            outs[k] = net.blobs[k].data[0,...]
-        fs.push(outs)    # Push current outputs.
-        # Elapsed time.
-        print 'Elapsed: {}'.format(time.time() - start)
-        ins = fs.pull()  # Fetch next inputs.
+        # Create ForwardScanner for the current dataset.
+        fs = ForwardScanner(dataset, scan_spec, params=scan_params)
 
+        # Scan loop.
+        ins = fs.pull()  # Fetch initial inputs.
+        while ins is not None:
+            start = time.time()
+            # Set inputs.
+            for k, v in ins.iteritems():
+                shape = (1,) + v.shape
+                net.blobs[k].reshape(*shape)
+                net.blobs[k].data[0,...] = v
+            # Run forward pass.
+            net.forward()
+            # Extract output data.
+            outs = dict()
+            for k in scan_spec.iterkeys():
+                outs[k] = net.blobs[k].data[0,...]
+            fs.push(outs)    # Push current outputs.
+            # Elapsed time.
+            print 'Elapsed: {}'.format(time.time() - start)
+            ins = fs.pull()  # Fetch next inputs.
+    return fs.outputs, save_prefix
+
+if __name__ == '__main__':
+    outputs, save_prefix = forward( sys.argv[2], device_id=int(sys.argv[1]) )
     # Save as file.
-    for key in fs.outputs.data.iterkeys():
-        fname = '{}_dataset{}_{}.h5'.format(save_prefix, idx+1, key)
+    for idx, key in enumerate(outputs.data.iterkeys(), 1):
+        fname = '{}_dataset{}_{}.h5'.format(save_prefix, idx, key)
         print 'Save {}...'.format(fname)
         f = h5py.File(fname)
-        output = fs.outputs.get_data(key)
+        output = outputs.get_data(key)
         f.create_dataset('/main', data=output)
         f.close()
